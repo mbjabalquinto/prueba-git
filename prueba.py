d975038dbca587d874fa8e11196bd31b2dc8e07b
Marcos Jabalquinto <mbjabalquinto@gmail.com>
2023-09-13 12:15:57 +0200

    añado comentario en línea diferente para ver si me deja hacer el merge.

--- prueba.py
+++ prueba.py
@@ -3,3 +3,5 @@ print("Prueba 2")
 print("Prueba 3 para probar el comando git diff antes de hacer un nuevo commit")
 print("Nuevo print para hacer un cambio en main y poder hacer merge con la otra rama")
 print("Añado nuevo print en la rama main para crear conflicto a propósito. Soy asín de chungo.")
+
+print("rama main")
